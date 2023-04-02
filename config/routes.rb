df31ffd4782b6fb116f2e8d7e4d4fc14b7917f82
yuki-nishimoto1109 <126940102+yuki-nishimoto1109@users.noreply.github.com>
2023-04-02 02:35:07 +0000

    [Add] Bootstrap/FontAwesome install

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+
+  root 'homes#top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
